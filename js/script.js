bf26d7c0166d253f5bd93c84903f8468a47bfabd
terrysmyth <twayburne@gmail.com>
2020-07-07 14:26:29 +0200

    fixes

--- js/script.js
+++ js/script.js
@@ -1,341 +1,33 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 const ul = document.querySelector("ul.student-list");
 const ulPag = document.querySelector("ul.pagination");
 const header = document.querySelector(".page-header");
 const searchResults = document.querySelector(".searchResults");
 let newList = [];
+let currentPage = 1;
 
 
-let currentPage = 0;
+// GET ALL STUDENTS FROM THE HTML AND STORE THEM
+let allList = document.getElementsByClassName("student-item");
+const list = [];
 
-const list = [{
-        name: "Terry Smith",
-        email: "twayburne@gmail.com",
-        joined: "07/15/15",
-        img: "https://randomuser.me/api/portraits/thumb/women/67.jpg"
-
-    },
-    {
-        name: "Brad",
-        email: "brad@gmail.com",
-        joined: "08/15/20",
-        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-
-    },
-    {
-        name: "Smith Ash",
-        email: "ash@gmail.com",
-        joined: "05/10/18",
-        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg"
-
-    },
-    {
-        name: "David",
-        email: "david@gmail.com",
-        joined: "01/21/01",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "Dawnna",
-        email: "dawnna@gmail.com",
-        joined: "08/08/08",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    }, {
-        name: "Terry",
-        email: "twayburne@gmail.com",
-        joined: "07/15/15",
-        img: "https://randomuser.me/api/portraits/thumb/women/67.jpg"
-
-    },
-    {
-        name: "Brad",
-        email: "brad@gmail.com",
-        joined: "08/15/20",
-        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-
-    },
-    {
-        name: "Ash Person",
-        email: "ash@gmail.com",
-        joined: "05/10/18",
-        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg"
-
-    },
-    {
-        name: "David",
-        email: "david@gmail.com",
-        joined: "01/21/01",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "Jess Heller",
-        email: "dawnna@gmail.com",
-        joined: "08/08/08",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    }, {
-        name: "Harry Person",
-        email: "twayburne@gmail.com",
-        joined: "07/15/15",
-        img: "https://randomuser.me/api/portraits/thumb/women/67.jpg"
-
-    },
-    {
-        name: "Brad",
-        email: "brad@gmail.com",
-        joined: "08/15/20",
-        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-
-    },
-    {
-        name: "Ash",
-        email: "ash@gmail.com",
-        joined: "05/10/18",
-        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg"
-
-    },
-    {
-        name: "Jake Smith",
-        email: "david@gmail.com",
-        joined: "01/21/01",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "Dawnna Smith",
-        email: "dawnna@gmail.com",
-        joined: "08/08/08",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    }, {
-        name: "Terry Wayburne",
-        email: "twayburne@gmail.com",
-        joined: "07/15/15",
-        img: "https://randomuser.me/api/portraits/thumb/women/67.jpg"
-
-    },
-    {
-        name: "Brad Donald",
-        email: "brad@gmail.com",
-        joined: "08/15/20",
-        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-
-    },
-    {
-        name: "Ash Smith",
-        email: "ash@gmail.com",
-        joined: "05/10/18",
-        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg"
-
-    },
-    {
-        name: "David Smith",
-        email: "david@gmail.com",
-        joined: "01/21/01",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "Dawnna Wayburne",
-        email: "dawnna@gmail.com",
-        joined: "08/08/08",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "David Wayburne-Smith",
-        email: "david@gmail.com",
-        joined: "01/21/01",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "Lovisa",
-        email: "something@gmail.com",
-        joined: "08/08/08",
-        img: "https://randomuser.me/api/portraits/thumb/men/62.jpg"
-
-    }, {
-        name: "Jeff Caller",
-        email: "twayburne@gmail.com",
-        joined: "07/15/15",
-        img: "https://randomuser.me/api/portraits/thumb/women/67.jpg"
-
-    },
-    {
-        name: "Brad Smither",
-        email: "brad@gmail.com",
-        joined: "08/15/20",
-        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-
-    },
-    {
-        name: "Ash",
-        email: "ash@gmail.com",
-        joined: "05/10/18",
-        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg"
-
-    },
-    {
-        name: "David",
-        email: "david@gmail.com",
-        joined: "01/21/01",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "Jennifer Smithy",
-        email: "jennifer@gmail.com",
-        joined: "08/08/08",
-        img: "https://randomuser.me/api/portraits/thumb/men/62.jpg"
-
-    }, {
-        name: "Terry",
-        email: "twayburne@gmail.com",
-        joined: "07/15/15",
-        img: "https://randomuser.me/api/portraits/thumb/women/67.jpg"
-
-    },
-    {
-        name: "Brad",
-        email: "brad@gmail.com",
-        joined: "08/15/20",
-        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-
-    },
-    {
-        name: "Ash Smith",
-        email: "ash@gmail.com",
-        joined: "05/10/18",
-        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg"
-
-    },
-    {
-        name: "David",
-        email: "david@gmail.com",
-        joined: "01/21/01",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "Dawnna Smith-Jones",
-        email: "dawnna@gmail.com",
-        joined: "08/08/08",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    }, {
-        name: "Terry",
-        email: "twayburne@gmail.com",
-        joined: "07/15/15",
-        img: "https://randomuser.me/api/portraits/thumb/women/67.jpg"
-
-    },
-    {
-        name: "Brad Aller-Smith",
-        email: "brad@gmail.com",
-        joined: "08/15/20",
-        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-
-    },
-    {
-        name: "Ash",
-        email: "ash@gmail.com",
-        joined: "05/10/18",
-        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg"
-
-    },
-    {
-        name: "David",
-        email: "david@gmail.com",
-        joined: "01/21/01",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "Dawnna Smith-Co",
-        email: "dawnna@gmail.com",
-        joined: "08/08/08",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    }, {
-        name: "Terry",
-        email: "twayburne@gmail.com",
-        joined: "07/15/15",
-        img: "https://randomuser.me/api/portraits/thumb/women/67.jpg"
-
-    },
-    {
-        name: "Brad",
-        email: "brad@gmail.com",
-        joined: "08/15/20",
-        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-
-    },
-    {
-        name: "Ash",
-        email: "ash@gmail.com",
-        joined: "05/10/18",
-        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg"
-
-    },
-    {
-        name: "Jake",
-        email: "david@gmail.com",
-        joined: "01/21/01",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "Dawnna",
-        email: "dawnna@gmail.com",
-        joined: "08/08/08",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    },
-    {
-        name: "Belinda",
-        email: "Belinda@gmail.com",
-        joined: "01/21/01",
-        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-
-    },
-    {
-        name: "Jess",
-        email: "dawnna@gmail.com",
-        joined: "08/08/08",
-        img: "https://randomuser.me/api/portraits/thumb/women/38.jpg"
-
-    }
-
-
-];
+for (var i = 0; i < allList.length; i++) {
+    let name = allList[i].querySelector("h3")
+    let img = allList[i].querySelector("img")
+    let joined = allList[i].querySelector(".date")
+    let email = allList[i].querySelector(".email")
+    list.push({
+        name: name.textContent,
+        email: email.textContent ,
+        img: img.src,
+        joined: joined.textContent
+    })
+}
 
 let currentList = list;
-
+console.log(list.length)
+console.log(list)
 
 // SHOWPAGE FUNCTION
-
 function showPage(page = 0, l) {
     ul.innerHTML = "";
 
@@ -359,32 +51,6 @@ function showPage(page = 0, l) {
 showPage(0, list);
 
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
 // CREATE LINKS
 function appendPageLinks(list) {
 
@@ -396,7 +62,7 @@ function appendPageLinks(list) {
         const li = document.createElement("li");
         const a = document.createElement("a");
 
-        if (i == currentPage) {
+        if (i == currentPage-1) {
 
             li.className = "current";
         } else {
@@ -404,27 +70,23 @@ function appendPageLinks(list) {
         }
 
         a.href = "#";
-        a.textContent = i;
+        a.textContent = i+1 ;
         li.appendChild(a);
-
-
         ulPag.appendChild(li);
     }
 
 }
-
 appendPageLinks(list);
 
-// SHOW DIFFERENT PAGE
 
+// SHOW DIFFERENT PAGE
 ulPag.addEventListener("click", (e) => {
     e.preventDefault();
     currentPage = e.target.textContent;
-    showPage(currentPage, currentList);
+    showPage(currentPage-1, currentList);
     appendPageLinks(currentList);
 
 })
-
 
 // Dynamically insert Form
 const searchDiv = document.createElement("div");
@@ -440,7 +102,6 @@ searchDiv.appendChild(searchButton);
 const searchResult = document.createElement("p");
 searchResults.appendChild(searchResult);
 
-
 searchDiv.addEventListener("click", (e) => {
 
     if (e.target.tagName == "BUTTON") {
@@ -451,25 +112,20 @@ searchDiv.addEventListener("click", (e) => {
             const search = searchInput.value;
             filterPage(search);
 
-
         } else {
             e.target.textContent = "Search";
             searchInput.value = "";
             searchResult.textContent = ``;
             currentList = list;
-            showPage(currentPage, currentList);
+            showPage(currentPage-1, currentList);
             appendPageLinks(currentList);
         }
-
-
-
     }
 
 });
 
-
+// FILTER
 function filterPage(search) {
-
 
     newList = [];
     for (var i = 0; i < list.length; i++) {
@@ -492,9 +148,3 @@ function filterPage(search) {
     appendPageLinks(currentList);
 
 }
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
